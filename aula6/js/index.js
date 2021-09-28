150d30ec6287d5c3e4682b0db4a579bbeb9e46f7
Caroline Vitória <carolmelo007@hotmail.com>
2021-09-28 16:28:01 -0300

    Variáveis com let

---
+++ aula6/js/index.js
@@ -0,0 +1,11 @@
+/* 
+a linguagem é case sensitive, diferencia letra minúscula de maiúscula
+Se utiliza camelCase
+não pode começar o nome de uma variável com número
+os nomes das variáveis não podem ter espaço, traço, ser palavras resservadas e precisam ter nomes significativos
+*/
+let nome; //variável declarada
+nome = 'korra'; //atribuição de valor
+alert(nome);
+let idade= 18;
+alert(idade);
