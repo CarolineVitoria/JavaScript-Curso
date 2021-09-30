dd4cd37d968db8090b3ca0e2a885dec1475e951c
Caroline Vitória <carolmelo007@hotmail.com>
2021-09-30 11:35:13 -0300

    Constantes

---
+++ aula7/js/index.js
@@ -0,0 +1,8 @@
+/*
+constantes também são espaços alocados na memória que recebem um valor, mas ao contrário das variáveis os dados atribuídos não podem ser alterados.
+também não podem ser declaradas e só depois inicializá-las com um valor como nas variáveis.
+*/
+const primeiroNumero = 3;
+const segundoNumero = 10;
+const calculo = primeiroNumero*segundoNumero;
+console.log(calculo);
