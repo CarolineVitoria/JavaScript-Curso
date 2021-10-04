d700ad887d4db548f768fe35e54ce8949fa5f1c9
Caroline Vitória <carolmelo007@hotmail.com>
2021-10-04 11:46:14 -0300

    Alert, confirm e prompt

---
+++ aula11/js/exercicio.js
@@ -0,0 +1,3 @@
+var numero1 = Number(prompt('Digite um número')); //convertendo para number junto com a entrada do dado
+var numero2 = Number(prompt('Digite outro número'));
+alert(`A soma desses números é: ${numero1+numero2}`);
---
+++ aula11/js/index.js
@@ -0,0 +1,4 @@
+alert('Sou o \nalert');
+confirm('Eu, o confirm, passo mensagens e retorno valores do tipo boolean');
+var idade = prompt('Eu, o prompt, recebo dados, mas os guardo sempre como strings. Qual a sua idade?');
+alert(`Vou mostrar a sua idade: ${idade}`);
