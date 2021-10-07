9d43b0f95c7dd78f226602c981e281d5d5c2bbd2
Caroline Vitória <carolmelo007@hotmail.com>
2021-10-07 12:28:49 -0300

    Exercício com strings

---
+++ aula13/exercicio/exe.js
@@ -0,0 +1,13 @@
+const nome = prompt('Digite seu nome completo: ');
+
+const excluindoEspaco = nome.replace(/\s/g, '');
+
+document.body.innerHTML += `Seu nome é: <strong>${nome}</strong> <br />`;
+document.body.innerHTML += `Seu nome tem <strong>${excluindoEspaco.length}</strong> letras <br />`;
+document.body.innerHTML += `A segunda letra do seu nome é: <strong>${nome.charAt(1)}</strong> <br />`;
+document.body.innerHTML += `Qual o primeiro índice da letra "i" no seu nome? <strong>${nome.indexOf('i')}</strong> <br />`;
+document.body.innerHTML += `Qual o último índice da letra "i" no seu nome? <strong>${nome.lastIndexOf('i')}</strong> <br />`;
+document.body.innerHTML += `As últimas 3 letras do seu nome são: <strong>${excluindoEspaco.slice(-3)}</strong> <br />`;
+document.body.innerHTML += `As palavras do seu nome são: <strong>${nome.split(' ')}</strong> <br />`;
+document.body.innerHTML += `Seu nome com letras maiúsculas: <strong>${nome.toUpperCase()}</strong> <br />`;
+document.body.innerHTML += `Seu nome com letras minúsculas: <strong>${nome.toLowerCase()}</strong> <br />`;
--- aula13/index.js
+++ aula13/index.js
@@ -9,5 +9,6 @@ console.log(umaString.indexOf('a', 3)) //o índice da letra 'a' depois do índic
 console.log(umaString.slice(0,3)); //retorna uma parte de uma string a partir dos índices
 console.log(umaString.slice(-11)); //vai retornar os caracteres a partir do tamanho da string menos o valor'11'
 console.log(umaString.replace(' ', ' meu ')); //substituir uma palavra/caractere por outro
+console.log(umaString.split(' ')) //divide a string
 console.log(umaString.toUpperCase()); //para deixar a string em maiúcula
 console.log(umaString.toLowerCase()); //para deixar em minúscula
