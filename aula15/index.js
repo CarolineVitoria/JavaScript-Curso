1586bc1c6538c1f508a2c4bead7c8ee0f146cd5e
Caroline Vitória <carolmelo007@hotmail.com>
2021-10-10 22:49:42 -0300

    Objeto Math

---
+++ aula15/index.js
@@ -0,0 +1,9 @@
+const num1 = 9.2342;
+console.log(Math.ceil(num1)); //arredonda pra cima\ sempre número inteiro
+console.log(Math.floor(num1)); //arredonda para baixo\ sempre inteiro
+console.log(Math.round(num1)); //arredonda para o número inteiro mais próximo
+console.log(Math.max(1, 240, 067, 1910)); //retorna o maior número de uma sequência
+console.log(Math.min(0.2, 60)); //retorna o menor número de uma sequência
+let aleatorio = Math.random(); //númeor aleatório entre 0 e 1, '1' não está incluído
+aleatorio = Math.round(Math.random() *(10 - 5) + 5); //número alatório inteiro entre 5 e 10
+console.log(aleatorio);
