c31bc89817f8d9651814df7c2cf3c338c0527294
Caroline Vitória <carolmelo007@hotmail.com>
2021-10-23 14:38:10 -0300

    Operadores de comparação

---
+++ aula21/index.js
@@ -0,0 +1,15 @@
+/*
+Operadores de comparação
+>maior que
+>= maior ou igual que
+< menor que
+<-= menor ou igual que
+== igual(valor)**n recomendado
+=== estritamente igual(valor e tipo)
+!= diferente(valor)**n recomendado
+!== estritamente diferente(valor e tipo)
+ */
+const num1 = 10;
+const num2 = 10;
+const comp = num1 === num2;
+console.log(comp);
