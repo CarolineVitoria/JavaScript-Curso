961a257fa61334f010715f97d12b2e85a285913a
Caroline Vitória <carolmelo007@hotmail.com>
2021-11-13 12:39:54 -0300

    for

---
+++ aula34/index.js
@@ -0,0 +1,12 @@
+for (let i=0; i<=5; i++){
+    console.log(`aé ${i}`);
+}
+for (let i=0; i<=10; i++){
+    const resultado = i % 2===0 ? `${i} é par` : `${i} é ímpar`;
+    console.log(resultado);
+}
+const frutas = ['Laranja', 'Goiaba', 'Uva'];
+
+for (let i = 0; i<frutas.length; i++){
+    console.log(frutas[i]);
+}
