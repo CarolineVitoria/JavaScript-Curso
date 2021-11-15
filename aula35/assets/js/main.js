bd1d24ac1b9aa0a0567784b9e8a3bf9934561773
Caroline Vitória <carolmelo007@hotmail.com>
2021-11-15 15:45:31 -0300

    Exercício com for

---
+++ aula35/assets/js/main.js
@@ -0,0 +1,18 @@
+function meuEscopo() {
+    const section = document.querySelector('section');
+    const divMae = document.createElement('div');
+    const elementos =[
+        { tag: 'p', texto: 'frase 1' },
+        { tag: 'div', texto: 'Frase 2' },
+        { tag: 'footer', texto: 'Frase 3' },
+        { tag: 'section', texto: 'Frase 3' },
+    ];
+    for(i=0; i< elementos.length; i++){
+        let {tag, texto}= elementos[i];
+        let elementoNovo = document.createElement(tag);
+        elementoNovo.innerText=texto;
+        divMae.appendChild(elementoNovo);
+    }
+    section.appendChild(divMae);
+} 
+meuEscopo();
