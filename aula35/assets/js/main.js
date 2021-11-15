260af1743fa471c1336594d9697345b9a57dbaa8
Caroline Vitória <carolmelo007@hotmail.com>
2021-11-15 15:47:09 -0300

    Correção na frase

--- aula35/assets/js/main.js
+++ aula35/assets/js/main.js
@@ -5,7 +5,7 @@ function meuEscopo() {
         { tag: 'p', texto: 'frase 1' },
         { tag: 'div', texto: 'Frase 2' },
         { tag: 'footer', texto: 'Frase 3' },
-        { tag: 'section', texto: 'Frase 3' },
+        { tag: 'section', texto: 'Frase 4' },
     ];
     for(i=0; i< elementos.length; i++){
         let {tag, texto}= elementos[i];
