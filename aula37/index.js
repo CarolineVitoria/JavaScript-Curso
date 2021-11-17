774e7569af693d09b640e60063ee9cb4cabd0bdf
Caroline Vitória <carolmelo007@hotmail.com>
2021-11-17 14:53:49 -0300

    For of

---
+++ aula37/index.js
@@ -0,0 +1,7 @@
+const nome = ['aang', 'korra', 'asami'];
+for(let valor of nome){
+    console.log(valor);
+}
+//for clássico - geralmentre dunciona com interáveis(stings, arrays)
+//for in - retorna o índice ou chave(string, arraye ou objetos)
+//for of - retorna o valor em si(iteráveis)
