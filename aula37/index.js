5b9e8d0eb49a4e59f13bf70827f186b809811db9
Caroline Vitória <carolmelo007@hotmail.com>
2021-11-17 17:56:05 -0300

    Correção no comentário

--- aula37/index.js
+++ aula37/index.js
@@ -2,6 +2,6 @@ const nome = ['aang', 'korra', 'asami'];
 for(let valor of nome){
     console.log(valor);
 }
-//for clássico - geralmentre dunciona com interáveis(stings, arrays)
+//for clássico - geralmentre funciona com interáveis(stings, arrays)
 //for in - retorna o índice ou chave(string, arraye ou objetos)
 //for of - retorna o valor em si(iteráveis)
