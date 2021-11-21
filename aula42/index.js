02a027b201e718fe532a3171421a1dfb75b64a38
Caroline Vitória <carolmelo007@hotmail.com>
2021-11-21 11:41:29 -0300

    Exercício imagem

---
+++ aula42/index.js
@@ -0,0 +1,4 @@
+//Uma função que receba dois argumentos, a altura e largura de uma imagem(number), e retorne true se estiver no modo paisagem
+const ePaisagem = (largura, altura) =>  largura>altura;
+
+console.log(ePaisagem(1080, 1920));
